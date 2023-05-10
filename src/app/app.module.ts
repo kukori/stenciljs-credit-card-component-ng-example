2be57464787f0a244f6cbd2c3e3f1a594c8d41b6
kukori <attila.varjas@gmail.com>
2023-05-10 11:08:52 +0200

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { CreditCardComponentsNgModule } from 'credit-card-components-angular/dist/credit-card-components-ng';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CreditCardComponentsNgModule
   ],
   providers: [],
   bootstrap: [AppComponent]
